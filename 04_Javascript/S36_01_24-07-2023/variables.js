20e9079c576e5186f185a2021a0ee13009bae9c9
Kabir <sagithedeveloper@gmail.com>
2023-07-24 08:34:57 +0530

    Added the code

---
+++ 04_Javascript/S36_01_24-07-2023/variables.js
@@ -0,0 +1,17 @@
+var id; // Variable Declaration
+
+id = 101; // Variable Assignment
+
+var city = "Delhi"; // Variable Initialization
+
+city = "Hyderabad"; // Re-Assignment
+
+var data = "s1";
+
+let username;
+
+username = "Riya Sharma";
+
+let usercity = "Pune";
+
+const pin = 32321;
